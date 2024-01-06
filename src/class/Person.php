11b929637e722eb298e1a1ac5780d482118cf0e9
etud user <etud@port-hp-probook650G8-2>
2024-01-06 11:45:06 +0100

    first

---
+++ src/class/Person.php
@@ -0,0 +1,17 @@
+<?php
+ 
+ class Person
+ {
+     public string $first_name;
+     // sexe : masculin => true, féminin => false
+     public bool $genre;
+  
+     public function __construct($first_name, $genre)
+     {
+         $this->first_name = $first_name;
+         $this->genre = $genre;
+     }
+ }
+  
+ ?>
+ 
